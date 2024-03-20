4a8ce0d4d2cb1ac093f5cf30971e627442ed3de6
Ilya Dybovsky <ilya77788899@mail.ru>
2024-03-20 20:50:03 +0300

    add payment without analitics

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,13 +1,19 @@
+import os
+import pyshorteners
+
 from aiogram import Router, types, F
 from aiogram.filters import CommandStart, Command, or_f
 from aiogram.utils.formatting import as_list, as_marked_section, Bold
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from database.orm_query import orm_get_products
+from database.orm_query import orm_get_product, orm_get_products
 
 from filters.chat_types import ChatTypeFilter
 
 from kbrds import reply
+from kbrds.inline import get_callback_btns
+
+import stripe
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
@@ -32,10 +38,32 @@ async def menu_command(message: types.Message, session: AsyncSession):
         await message.answer_photo(
             product.image,
             caption=f"Название: <strong>{product.name}</strong>\nОписание: {product.description}\nСтоимость: {round(product.price, 2)}",
-            reply_markup=reply.del_kb
+            reply_markup=get_callback_btns(btns={"Купить": f"buyProduct_{product.id}"}, sizes=(1,))
         )
 
+@user_private_router.callback_query(F.data.startswith("buyProduct_"))
+async def buy_product(callback: types.CallbackQuery, session: AsyncSession):
+    product_id = callback.data.split("_")[-1]
 
+    product = await orm_get_product(session, product_id=int(product_id))
+
+    stripe.api_key = os.getenv("STRIPE_SK")
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        success_url="https://web.telegram.org/a/#6312125642",
+        line_items=[
+            {
+                "price_data":{
+                    "currency": "rub",
+                    "unit_amount": int(product.price) * 100,
+                    "product_data": {"name": str(product.name), "description": str(product.description)},
+                },
+                "quantity": 1,
+            },
+        ],
+        mode="payment",
+    )
+    await callback.message.answer(f"Оплата тут:\n"+str(pyshorteners.Shortener().clckru.short(checkout_session["url"])))
 
 @user_private_router.message(F.text.lower() == "о нас")
 @user_private_router.message(Command("about"))
